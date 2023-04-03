4aa69faefe4e19af17fa0d0c54f50bf4d7f97e9c
ooxofficial <monteonee@gmail.com>
2023-04-03 16:04:38 +0200

    preview update

--- install.sh
+++
@@ -1,7 +0,0 @@
-dir=$(pwd)
-cd /usr/local/bin/
-sudo curl -s https://raw.githubusercontent.com/ooxofficial/ooxfetch --output maxfetch
-sudo chmod +x /usr/local/bin/maxfetch
-echo "Done!"
-ooxfetch
-cd $dir
